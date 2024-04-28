bb8f687dc4f656b677f3d1f3bdd3c55e23036f89
ITalant7 <64712032+Tako0502@users.noreply.github.com>
2024-04-28 14:58:05 +0500

    1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
-document.addEventListener("DOMContentLoaded",function (){
-  new TomSelect("#city",{
-    create: true,
-    sortField: {
-      field: "text",
-      direction: "asc"
+document.addEventListener("DOMContentLoaded", function () {
+  let reactions = document.querySelectorAll('.reaction');
+  reactions.forEach(e => {
+    e.onclick = function () {
+      let icon = e.querySelector("i");
+      reactions.forEach(el => {
+        let i = el.querySelector("i");
+        i.classList.remove("fa-solid");
+        i.classList.remove(el.getAttribute("id").toString());
+      });
+      icon.classList.toggle(e.getAttribute("id").toString());
+      icon.classList.toggle("fa-solid");
     }
+  });
+});
+document.addEventListener("DOMContentLoaded", async function () {
+  const choices = new Choices(document.querySelector('#categoryId'), {
+    placeholder: false,
+    itemSelectText: '',
+    searchEnabled: false
+
   });
 });
