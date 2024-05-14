25c1b25ecdc424dd3249387666b53d6e9d3bcd44
ITalant7 <64712032+Tako0502@users.noreply.github.com>
2024-05-15 03:32:50 +0500

    121

--- js/backtotop.js
+++ js/backtotop.js
@@ -1,31 +1,35 @@
-(function(){
-    // Back to Top - by CodyHouse.co
-	var backTop = document.getElementsByClassName('js-cd-top')[0],
-		offset = 300, // browser window scroll (in pixels) after which the "back to top" link is shown
-		offsetOpacity = 1200, //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-		scrollDuration = 700,
-		scrolling = false;
-
-	if( backTop ) {
-		//update back to top visibility on scrolling
-		window.addEventListener("scroll", function(event) {
-			if( !scrolling ) {
-				scrolling = true;
-				(!window.requestAnimationFrame) ? setTimeout(checkBackToTop, 250) : window.requestAnimationFrame(checkBackToTop);
-			}
-		});
-
-		//smooth scroll to top
-		backTop.addEventListener('click', function(event) {
-			event.preventDefault();
-			(!window.requestAnimationFrame) ? window.scrollTo(0, 0) : Util.scrollTo(0, scrollDuration);
-		});
-	}
-
-	function checkBackToTop() {
-		var windowTop = window.scrollY || document.documentElement.scrollTop;
-		( windowTop > offset ) ? Util.addClass(backTop, 'cd-top--is-visible') : Util.removeClass(backTop, 'cd-top--is-visible cd-top--fade-out');
-		( windowTop > offsetOpacity ) && Util.addClass(backTop, 'cd-top--fade-out');
-		scrolling = false;
-	}
-})();
+!function(s) {
+    "use strict";
+    s(".switch").on("click", function() {
+        s("body").hasClass("light") ? (s("body").removeClass("light"),
+        s(".switch").removeClass("switched")) : (s("body").addClass("light"),
+        s(".switch").addClass("switched"))
+    }),
+    s(document).ready(function() {
+        var e = document.querySelector(".progress-wrap path")
+          , t = e.getTotalLength();
+        e.style.transition = e.style.WebkitTransition = "none",
+        e.style.strokeDasharray = t + " " + t,
+        e.style.strokeDashoffset = t,
+        e.getBoundingClientRect(),
+        e.style.transition = e.style.WebkitTransition = "stroke-dashoffset 10ms linear";
+        var o = function() {
+            var o = s(window).scrollTop()
+              , r = s(document).height() - s(window).height()
+              , i = t - o * t / r;
+            e.style.strokeDashoffset = i
+        };
+        o(),
+        s(window).scroll(o);
+        jQuery(window).on("scroll", function() {
+            jQuery(this).scrollTop() > 50 ? jQuery(".progress-wrap").addClass("active-progress") : jQuery(".progress-wrap").removeClass("active-progress")
+        }),
+        jQuery(".progress-wrap").on("click", function(s) {
+            return s.preventDefault(),
+            jQuery("html, body").animate({
+                scrollTop: 0
+            }, 550),
+            !1
+        })
+    })
+}(jQuery);
--- js/main.js
+++ js/main.js
@@ -1,76 +1,41 @@
+var owl = $('.ul-craousel');
 
 document.addEventListener("DOMContentLoaded", function () {
-  let reactions = document.querySelectorAll('.reaction');
-  reactions.forEach(e => {
-    e.onclick = function () {
-      let icon = e.querySelector("i");
-      reactions.forEach(el => {
-        let i = el.querySelector("i");
-        i.classList.remove("fa-solid");
-        i.classList.remove(el.getAttribute("id").toString());
-      });
-      icon.classList.toggle(e.getAttribute("id").toString());
-      icon.classList.toggle("fa-solid");
-    }
+  owl.owlCarousel({
+    items: 1,
+    loop: true,
+    smartSpeed: 1000,
+    transitionStyle: "fade",
+    animateOut: 'fadeOut',
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    onInitialized: startAutoplay
   });
-});
-document.addEventListener("DOMContentLoaded", async function () {
-  const choices = new Choices(document.querySelector('#categoryId'), {
-    placeholder: false,
-    itemSelectText: '',
-    searchEnabled: false
+
+  $('.pinned-article-info').on('click', function() {
+    var index = $(this).index();
+    owl.trigger('to.owl.carousel', index);
+    owl.trigger('play.owl.autoplay', [3000]);
+  });
+
+  const collapsibleHeader = document.querySelectorAll('.ul-navigation-bar .dropdown');
+  if(collapsibleHeader)
+    {
+        collapsibleHeader.forEach(element => {
+      element.addEventListener('mouseenter', function () {
+          this.querySelector(".arrow-r")?.classList.add("active");
+
+          element.addEventListener('mouseleave', function () {
+              this.querySelector(".arrow-r")?.classList.remove("active");
+          });
+      });
 
   });
-});
-document.addEventListener("DOMContentLoaded", function () {
-  const initPartnerSwiper = function () {
-    if (window.innerWidth < 768) {
-      partnerSwiper = new Swiper(".alnews-swiper", {
-        slidesPerView: 2,
-        spaceBetween: 16,
-
-        freeMode: true,
-        direction: 'horizontal',
-        loop: true,
-
-        pagination: {
-          el: '.swiper-pagination',
-        },
-      });
-    } else if (window.innerWidth >= 768 && window.innerWidth <= 1024) {
-      partnerSwiper = new Swiper(".swiper", {
-        slidesPerView: 2,
-        spaceBetween: 16,
-
-        freeMode: true,
-        direction: 'horizontal',
-        loop: true,
-
-        pagination: {
-          el: '.swiper-pagination',
-        },
-      });
-    } else {
-      partnerSwiper = new Swiper(".swiper", {
-        slidesPerView: 3,
-        spaceBetween: 16,
-        freeMode: true,
-        direction: 'horizontal',
-        loop: true,
-
-        pagination: {
-          el: '.swiper-pagination',
-        },
-        //autoplay: {
-        //    delay: 3000,
-        //},
-      });
     }
-  };
-  initPartnerSwiper();
-
-
-
 
 });
 
+function startAutoplay() {
+  owl.trigger('play.owl.autoplay', [3000]);
+}
